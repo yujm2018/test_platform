b150e55e1078e0ac4884e36e2e99a8f37cc6e04b
yujm2018 <527828071@qq.com>
2019-03-01 23:53:49 +0800

    修改提交

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,6 +1,1 @@
 # encoding: utf-8
-"""
-@author: yujianming
-@file: urls.py
-@time: 2019/2/25 19:53
-"""
--- user_app/views.py
+++ user_app/views.py
@@ -4,8 +4,6 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
 
-
-# Create your views here.
 def index(request):
     return render(request,"index.html")
 
