9736e63027e87654109d622b09b4a19e6439a67d
yujm2018 <527828071@qq.com>
2019-03-01 16:53:43 +0800

    修改提交

--- project_app/views.py
+++ project_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from project_app.models import Project,Module
 
-# Create your views here.
 @login_required()   #判断用户是否登录
 def project_manage(request):
     username = request.session.get('user1','') #读取浏览器session
