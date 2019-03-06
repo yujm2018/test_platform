1bc593f1a6d672ccd81c3398746962ccfa450de9
yujm2018 <527828071@qq.com>
2019-03-06 19:29:50 +0800

    新增用例表

--- interface_app/admin.py
+++ interface_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from interface_app.models import TestCase
 
-# Register your models here.
+
+class TestCaseAdmin(admin.ModelAdmin):
+    list_display = ["project","module","name","url","req_method","req_type","req_header","req_parameter","response_assert"]
+
+admin.site.register(TestCase,TestCaseAdmin)
--- interface_app/models.py
+++ interface_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from project_app.models import Project,Module
 
-# Create your models here.
+class TestCase(models.Model):
+    """
+    用例表
+    """
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    name = models.CharField("名称", max_length=100, blank=False, default="")
+    url = models.TextField("URL", default="")
+    req_method = models.CharField("方法", max_length=10, default="")
+    req_type = models.CharField("参数类型", max_length=10, default="")
+    req_header = models.TextField("请求头", default="")
+    req_parameter = models.TextField("请求参数", default="")
+    response_assert = models.TextField("验证", default="")
+    create_time = models.DateTimeField("创建时间", auto_now=True)
+
+    def __str__(self):
+        return self.name
